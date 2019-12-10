7dcbbe8c4e8cbf384e694aacb89c1e93f8825217
Christian Neumann <cneumann@e3code.de>
2019-12-10 15:15:49 +0100

    Fix typos.

--- js/ui/recipe/green-hosting.js
+++ js/ui/recipe/green-hosting.js
@@ -7,17 +7,17 @@ class GreenHosting {
   constructor() {
     this.visible = true;
     this.id = 'green_hosting';
-    this.name = __( 'Use a green hoster', 'greenerwp' );
+    this.name = __( 'Use a green host', 'greenerwp' );
     this.description =  __( 'Host your WordPress site on a host that uses renewable energy for its servers.', 'greenerwp' ) + ' ' +
-			__( 'Have a look at the [Green Web Foundations\' green hoster directory](https://www.thegreenwebfoundation.org/directory/) to find a green host.', 'greenerwp' );
+			__( 'Have a look at the [Green Web Foundations\' green host directory](https://www.thegreenwebfoundation.org/directory/) to find a green host.', 'greenerwp' );
 		this.guide = "hosting";
 		this.steps = [
       {
         type: 'text',
-        id: 'use_green_hoster',
+        id: 'use_green_host',
 				check: (analysis) => { return analysis.is_green_host; },
         args: {
-          text: __( 'Use a hoster with renewable energy.', 'greenerwp' ),
+          text: __( 'Use a host with renewable energy.', 'greenerwp' ),
 					description: __( 'The recipe will be marked as done if your host is in the Green Web Foundation\'s database.', 'greenerwp' ) + ' ' +
 						__( 'Will be checked at most once every day.', 'greenerwp' ),
         },
